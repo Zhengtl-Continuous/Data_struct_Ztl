cc44aea259d485bf7b9b35702e7bda35eb73575b
zhengtl <3465868616@qq.com>
2025-09-18 09:07:18 +0800

    Update and rename list to List.cpp

---
+++ List.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+typedef int ElemType;
+typedef struct ListNode
+{
+	ElemType data;
+	ListNode *prior;
+	ListNode *next;
+}DlinkNode;
+
+
+
+int main()
+{
+	cout<<"Hello World"<<endl;
+	return 0;
+}
